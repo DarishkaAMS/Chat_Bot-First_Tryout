9dbdc4700fc430e2b2d95ae9b57a0df992d87546
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-01-30 12:04:06 +0200

    Set environment

---
+++ train_chatbot.py
@@ -0,0 +1,16 @@
+import nltk
+from nltk.stem import WordNetLemmatizer
+import json
+import pickle
+import numpy as np
+# from keras.models import Sequential
+# from keras.layers import Dense, Activation, Dropout
+# from keras.optimizers import SGD
+# import random
+
+
+lemmatizer = WordNetLemmatizer
+
+words, classes, documents = [], [], []
+
+print(words, classes, documents)
